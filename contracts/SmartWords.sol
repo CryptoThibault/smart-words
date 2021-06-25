32bbd2e39bf90355c98577c52f32a1db40a5cc3b
CryptoThibault <thibault.chalaoux@gmail.com>
2021-06-25 14:41:45 +0200

    struct text

--- contracts/SmartWords.sol
+++ contracts/SmartWords.sol
@@ -1,33 +1,63 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
 
-import "./CustomNFT.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract SmartWords is AccessControl {
-  mapping(address => mapping(uint => CustomNFT)) private _nfts;
-  mapping(uint => address) private _nftOwners;
-  uint private _countId;
+struct Text {
+  string text;
+  address author;
+  uint timestamp;
+  bytes32 key;
+}
 
-  function addressNftOf(address owner, uint id) public view returns (address) {
-    return address(_nfts[owner][id]);
+contract SmartWords is ERC721, AccessControl {
+  mapping(uint => Text) private _texts;
+  uint private _id;
+
+  constructor() ERC721("Words Token", "WRD") {
+    
+  }
+  
+  modifier onlyTextOwner(uint id) {
+    require(msg.sender == ownerOf(id), "SmartWords: function reserved to owner of this text");
+    _;
   }
 
-  function addressNftOwner(uint id) public view returns (address) {
-    return _nftOwners[id];
+  function textContent(uint id_) public view returns(string memory) {
+    return _texts[id_].text;
+  }
+  function textAuthor(uint id_) public view returns(address) {
+    return _texts[id_].author;
+  }
+  function textOwner(uint id_) public view returns(address) {
+    return ownerOf(id_);
+  }
+  function textTimestamp(uint id_) public view returns (uint) {
+    return _texts[id_].timestamp;
+  }
+  function textKey(uint id_) public view returns(bytes32) {
+    return _texts[id_].key;
   }
 
-  function codeNftOf(address owner, uint id) public view returns (bytes32) {
-    return _nfts[owner][id].code();
+
+
+  function write(string memory text_) public {
+    _mint(msg.sender, _id);
+    _createText(_id, text_);
+    _id++;
   }
 
-  function textNftOf(address owner, uint id) public view returns (string memory) {
-    return _nfts[owner][id].text();
+  function edit(uint id_, string memory text_) public onlyTextOwner(id_) {
+    _createText(id_, text_);
   }
 
-  function _deployNFT(address owner, string memory text) private {
-    _nfts[owner][_countId] = new CustomNFT(owner, _countId, text);
-    _nftOwners[_countId] = owner;
-    _countId++;
+  function _createText(uint id_, string memory text_) private {
+    _texts[id_] = Text({
+      text: text_,
+      author: msg.sender,
+      timestamp: block.timestamp,
+      key: keccak256("KEY")
+    });
   }
 }
